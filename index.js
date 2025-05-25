4ef0332b25e9cd0fb05f7f56b0f1ea6315b99a56
AHMEDTASRIFABDULLAH <tasrifabdullah234@gmail.com>
2025-05-25 13:11:04 +0600

    crud operations done

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 require("dotenv").config();
 app.use(express.json());
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const corsOptions = {
   origin: ["http://localhost:5173"],
   credentials: true,
@@ -40,7 +40,6 @@ async function run() {
       const result = await usersCollection.insertOne(user);
       res.send(result);
     });
-
     app.post("/login", async (req, res) => {
       const { email, password } = req.body;
       const user = await usersCollection.findOne({ email });
@@ -55,9 +54,65 @@ async function run() {
       }
       res.send({ message: "Login successful", user });
     });
-
     app.get("/users", async (req, res) => {
       const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+    app.delete("/delete", async (req, res) => {
+      const ids = req.body.ids;
+      const objectIds = ids.map((id) => new ObjectId(id));
+      const result = await usersCollection.deleteMany({
+        _id: { $in: objectIds },
+      });
+      res.send(result);
+    });
+    app.patch("/blocked", async (req, res) => {
+      const { ids, status } = req.body;
+      if (!ids || !Array.isArray(ids) || ids.length === 0) {
+        return res
+          .status(400)
+          .send({ message: "Please select users to block" });
+      }
+      const objectIds = ids.map((id) => new ObjectId(id));
+      const alreadyBlocked = await usersCollection.findOne({
+        _id: { $in: objectIds },
+        status: "blocked",
+      });
+
+      if (alreadyBlocked) {
+        return res.status(403).send({
+          message: `Some selected user's are already blocked. Cannot update.`,
+        });
+      }
+      const result = await usersCollection.updateMany(
+        { _id: { $in: objectIds } },
+        { $set: { status: status } }
+      );
+
+      res.send(result);
+    });
+    app.patch("/unblocked", async (req, res) => {
+      const { ids, status } = req.body;
+      if (!ids || !Array.isArray(ids) || ids.length === 0) {
+        return res
+          .status(400)
+          .send({ message: "Please select users to active" });
+      }
+      const objectIds = ids.map((id) => new ObjectId(id));
+      const alreadyActive = await usersCollection.findOne({
+        _id: { $in: objectIds },
+        status: "active",
+      });
+
+      if (alreadyActive) {
+        return res.status(403).send({
+          message: `Some selected user's are already Active. Cannot update.`,
+        });
+      }
+      const result = await usersCollection.updateMany(
+        { _id: { $in: objectIds } },
+        { $set: { status: status } }
+      );
       res.send(result);
     });
   } finally {
